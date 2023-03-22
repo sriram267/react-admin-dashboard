c58579012be8952e97e4a42e309839020821d42e
sriram <vssriram7@gmail.com>
2023-03-22 23:31:57 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { ColorModeContext, useMode} from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./screens/global/Topbar";
+import Dashboard  from "./screens/dashboard";
+import Sidebar from "./screens/global/Sidebar";
+// import Team  from "./screens/team";
+// import Invoices  from "./screens/invoices";
+// import Contacts  from "./screens/contacts";
+// import Bar  from "./screens/bar";
+// import Form  from "./screens/form";
+// import Line  from "./screens/line";
+// import Pie  from "./screens/pie";
+// import FAQ  from "./screens/fAQ";
+// import Geography  from "./screens/geography";
+// import Callendar  from "./screens/callendar";
+ 
+const App = () => {
+  const [theme , colorMode] = useMode();
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              {/* <Route path="/" element={<Dashboard />} />
+              <Route path="/Sidebar" element={<Sidebar />} /> */}
+              {/* <Route path="/Team" element={<Team />} />
+              <Route path="/Invoices" element={<Invoices />} />
+              <Route path="/Contacts" element={<Contacts />} />
+              <Route path="/Bar" element={<Bar />} />
+              <Route path="/Form" element={<Form />} />
+              <Route path="/Line" element={<Line />} />
+              <Route path="/Pie" element={<Pie />} />
+              <Route path="/FAQ" element={<FAQ />} />
+              <Route path="/Geography" element={<Geography />} />
+              <Route path="/Callendar" element={<Callendar />} /> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+    
+  
+  )
 }
 
 export default App;
+
+
