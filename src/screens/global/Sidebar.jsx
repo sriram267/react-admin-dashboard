3cb3bedec274d63b87e569c4bd10c308d01a5377
sriram <vssriram7@gmail.com>
2023-03-27 15:37:30 +0530

    .

--- src/screens/global/Sidebar.jsx
+++ src/screens/global/Sidebar.jsx
@@ -45,7 +45,7 @@ const Sidebar = () => {
                 backgroundColor: "transparent !important",
               },
               "& .pro-inner-item": {
-                padding: "5px 33px 5px 20px !important",
+                padding: "5px 33px 5px 18px !important",
               },
               "& .pro-inner-item:hover": {
                 color: "#868dfb !important",
